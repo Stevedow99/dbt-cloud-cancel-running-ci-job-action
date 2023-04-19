f569d8e1f0b641af7aec7c2e3bf0882068d2b34b
Soonho Kwon <78287018+skwon615@users.noreply.github.com>
2023-04-19 10:50:01 -0700

    Fix int type mismatch bug

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def extract_dbt_runs_info(recent_runs_list, job_id, same_branch_flag, use_schema
                 # grabbing the schema override to parse PR number when CI is triggered by external process
                 run_schema_override = run['trigger']['schema_override']
                 schema_override_prefix = f'dbt_cloud_pr_{str(job_id)}_'
-                run_git_pr_number = None if run_schema_override is None else run_schema_override.lstrip(schema_override_prefix)
+                run_git_pr_number = None if run_schema_override is None or not run_schema_override.isnumeric() else int(run_schema_override.lstrip(schema_override_prefix))
             
             else:
 
